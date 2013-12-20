55098e696443d6234b2606a7da35d31de8f1ecdb
tinybeans <register@tinybeans.net>
2013-12-20 22:54:37 +0900

    Added "flexibleSearch-config.js"

---
+++ flexibleSearch-config.js
@@ -0,0 +1,50 @@
+(function ($) {
+    $("#search").flexibleSearch({
+        // // Path
+        // searchDataPath: "/flexibleSearch/search.json",
+        // searchDataPathPreload: "/flexibleSearch/search.json",
+
+        // // Data API
+        // dataApiDataIds: null,
+        // dataApiParams: null,
+
+        // // Performance
+        // cache:  true, // I recommend "true".
+
+        // // Search Form
+        // searchFormCreation: true,
+        // searchFormHTML: null,
+        // searchFormAction: "",
+        // searchFormInputType: "search",
+        // searchFormInputPlaceholder: "Search words",
+        // searchFormSubmitBtnText: "Search",
+
+        // // Advanced Search Form
+        // advancedFormObj: null,
+
+        // // Result Block
+        // loadingImgPath: "/flexibleSearch/loading.gif",
+        // loadingImgHTML: null,
+        // resultBlockId: "fs-result",
+        // resultMsgTmpl: null,
+        // resultItemTmpl: null,
+
+        // // Paginate
+        // paginateId: "fs-paginate",
+        // paginateTmpl: null,
+        // paginateCount: 10,
+
+        // // Submit
+        // submitAction: function (paramArray) {
+        //     return paramArray;
+        // },
+
+        // // Ajax
+        // ajaxError: function (jqXHR, textStatus, errorThrown) {
+        //     window.alert(textStatus);
+        // },
+
+        // excludeParams: null,
+        dummy: false
+    });
+})(jQuery);
